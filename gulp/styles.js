dc4636b4dc213d798c29c22373ed6232ec88bd6f
Ronan Burke <ronan.burke@civica.co.uk>
2018-12-31 23:47:42 +0000

    Add gulp build task + add humans.txt to gulp static

--- gulp/styles.js
+++ gulp/styles.js
@@ -8,20 +8,10 @@ var autoprefixer = require('gulp-autoprefixer'),
     rename = require('gulp-rename'),
     sass = require('gulp-sass');
 
-gulp.task('styles', ['styles:build'], function() {
+gulp.task('styles', function() {
     gulp.src(conf.paths.src + '/styles/*.scss')
         .pipe(rename({ suffix: '.min' }))
         .pipe(sass())
         .pipe(gulp.dest(conf.paths.dist + '/css'))
         .pipe(cssnano());
-});
-
-gulp.task('styles:build', function() {
-	gulp.watch(conf.paths.src + '/styles/*.scss', ['sass']);
-    /*return sass('src/main/styles/neo4jd3.scss', { style: 'expanded' })
-        .pipe(autoprefixer('last 2 version'))
-        .pipe(gulp.dest(conf.paths.docs + '/css'))
-        
-        .pipe(gulp.dest(conf.paths.docs + '/css'))
-        .pipe(connect.reload());*/
 });
