6df2d6294ece11c0d41a8aefc433b09cd63441f2
abuDarda97 <abudarda166@gmail.com>
2019-04-13 23:45:08 +0100

    gulp bug fixed

--- gulp/build.js
+++ gulp/build.js
@@ -4,5 +4,5 @@ var gulp = require('gulp'),
     runSequence = require('run-sequence');
 
 gulp.task('build', function(callback) {
-    runSequence('clean', 'images', 'scripts', 'styles', 'node_modules', 'static', callback);
+    runSequence('clean', 'images', 'scripts', 'node_modules', 'static', callback);
 });
