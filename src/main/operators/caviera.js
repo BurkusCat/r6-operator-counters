a7d037f86103ab7802cba1b2f81bf9a12ce4372c
Mr. Martinez <jrequema@medxopensystems.com>
2020-03-09 20:29:37 -1000

    fix typo errors on operator names

--- src/main/operators/caviera.js
+++ src/main/operators/caviera.js
@@ -3,6 +3,6 @@ import operatorId from './core/operatorIdEnum';
 import Operator from './core/operatorFactory';
 import r6operators from "r6operators";
 
-let caviera = new Operator(r6operators.caviera, operatorId.caviera, "Operation Skull Rain");
+let caviera = new Operator(r6operators.caveira, operatorId.caviera, "Operation Skull Rain");
 
 export default caviera
--- src/main/operators/recruit.js
+++ src/main/operators/recruit.js
@@ -3,6 +3,6 @@ import operatorId from './core/operatorIdEnum';
 import Operator from './core/operatorFactory';
 import r6operators from "r6operators";
 
-let recruit = new Operator(r6operators.recruit, operatorId.recruit, "N/A");
+let recruit = new Operator(r6operators.recruit_blue, operatorId.recruit, "N/A");
 
 export default recruit
