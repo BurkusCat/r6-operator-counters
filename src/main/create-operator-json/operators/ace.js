072586ad4e83becad1a83d300b9e78b095ed4258
Ronan Burke <burkuscat@gmail.com>
2020-07-19 12:57:23 +0100

    Add basic Ace counters

--- src/main/create-operator-json/operators/ace.js
+++ src/main/create-operator-json/operators/ace.js
@@ -7,4 +7,6 @@ import r6operators from "r6operators";
 
 let ace = new Operator(r6operators.ace, operatorId.ace, "Operation Steel Wave");
 
+ace.addCounterNode(operatorId.mira, counterType.hard, "Ace's SELMA Breaching Device can destroy Mira's Black Mirror.");
+
 export default ace
--- src/main/create-operator-json/operators/mute.js
+++ src/main/create-operator-json/operators/mute.js
@@ -9,6 +9,7 @@ let mute = new Operator(r6operators.mute, operatorId.mute, "N/A");
 
 mute.addCounterNode(operatorId.hibana, counterType.hard, "Mute's Signal Disruptor can stop Hibana's X-KARIOS pellets from triggering.");
 mute.addCounterNode(operatorId.thermite, counterType.hard, "Mute's Signal Disruptor can stop Thermite's Exothermic Charge from triggering.");
+mute.addCounterNode(operatorId.ace, counterType.hard, "Mute's Signal Disruptor can stop Ace's SELMA Breaching Device from triggering.");
 mute.addCounterNode(operatorId.fuze, counterType.hard, "Mute's Signal Disruptor can stop Fuze's Cluster Charges from triggering.");
 mute.addCounterNode(operatorId.twitch, counterType.hard, "If placed correctly, Mute's Signal Disruptor can disrupt Twitch's Drone.");
 mute.addCounterNode(operatorId.dokkaebi, counterType.hard, "When standing within range of Mute's Signal Disruptor, a defender's phone will not ring when Dokkaebi activates her Logic Bomb. A ringing phone will stop if you step within range of a Signal Disruptor.");
