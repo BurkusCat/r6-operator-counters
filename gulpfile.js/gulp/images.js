c88e85eb1cb85bde98f9226c1b10c7d93e7e9f16
Ronan Burke <burkuscat@gmail.com>
2022-05-08 00:03:17 +0100

    Initial PWA codebase

--- gulpfile.js/gulp/images.js
+++ gulpfile.js/gulp/images.js
@@ -5,11 +5,18 @@ var conf = require('./conf'),
     gulp = require('gulp'),
     imagemin = require('gulp-imagemin');
 
-function images() {
-    return gulp.src(conf.paths.src + '/images/**/*')
+function site_images() {
+    return gulp.src(conf.paths.src + '/images/site/*')
         .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
-        .pipe(gulp.dest(conf.paths.dist + '/img'))
+        .pipe(gulp.dest(conf.paths.dist + '/img/site'))
         .pipe(connect.reload());
 };
 
+function appicon_images() {
+    return gulp.src(conf.paths.src + '/images/appicons/**/*')
+        .pipe(gulp.dest(conf.paths.dist + '/img/appicons'))
+        .pipe(connect.reload());
+};
+
+const images = gulp.series(site_images, appicon_images);
 exports.default = images;
