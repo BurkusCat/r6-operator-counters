75fb3074a2b8dcfd848b4c738ffb0890a57cf4ca
connor <connor.botterill@yahoo.co.uk>
2022-10-04 20:06:47 +0100

    feat: add pulse hard counter for maverick

--- src/main/create-operator-json/operators/pulse.js
+++ src/main/create-operator-json/operators/pulse.js
@@ -8,5 +8,6 @@ import r6operators from "r6operators";
 let pulse = new Operator(r6operators.pulse, operatorId.pulse, "N/A");
 
 pulse.addCounterNode(operatorId.finka, counterType.soft, "Pulse's Heartbeat Sensor will detect Attackers boosted by Finka's Adrenal Surge for twice the amount of time.");
+pulse.addCounterNode(operatorId.maverick, counterType.hard, "Pulse's HeartBeat Sensor will detect Maverick when attempting to put holes through walls.");
 
 export default pulse
