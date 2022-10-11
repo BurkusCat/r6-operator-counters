1ec5d97d92479935dafba00fa934196f34a38c96
connor <connor.botterill@yahoo.co.uk>
2022-10-11 08:43:59 +0100

    feat: add maestro minor counter for blitz

--- src/main/create-operator-json/operators/maestro.js
+++ src/main/create-operator-json/operators/maestro.js
@@ -14,6 +14,7 @@ maestro.addCounterNode(operatorId.twitch, counterType.hard, "Maestro's Evil Eye 
 maestro.addCounterNode(operatorId.glaz, counterType.minor, "Maestro's Evil Eye can detect Glaz through smoke, which Glaz relies on.")
 maestro.addCounterNode(operatorId.maverick, counterType.soft, "Maestro's Evil Eye can shoot Maverick with its laser, making it difficult for him to blow torch holes.")
 maestro.addCounterNode(operatorId.montagne, counterType.minor, "Maestro's Evil Eye can shoot Montagne from behind when he is pushing, causing him to turn around and potentially exposing him.")
+maestro.addCounterNode(operatorId.blitz, counterType.minor, "Maestro's Evil Eye can shoot Blitz from behind when he is hard pushing, causing him to turn around and potentially exposing him.")
 
 
 export default maestro
