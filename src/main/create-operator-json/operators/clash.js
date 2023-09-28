b9f00587ada437602761480c1ee565a09329eb81
AustinGallippo <127637468+AustinGallippo@users.noreply.github.com>
2023-09-28 12:11:35 -0400

    Update clash.js
    Updated montagne text to better represent the counter between them.

--- src/main/create-operator-json/operators/clash.js
+++ src/main/create-operator-json/operators/clash.js
@@ -7,6 +7,6 @@ import r6operators from "r6operators";
 
 let clash = new Operator(r6operators.clash, operatorId.clash, "Operation Grim Sky");
 
-clash.addCounterNode(operatorId.montagne, counterType.soft, "Montagne is more susceptible to clash's shocks.");
+clash.addCounterNode(operatorId.montagne, counterType.soft, "Montagne is more susceptible to shocks from Clash's CCE Shield.");
 
 export default clash
