410bc09909d18ded5d5b0a00a48376e99bd1a904
lily <lilyhuang-github@users.noreply.github.com>
2024-09-27 12:00:28 -0400

    fix typo

--- src/main/create-operator-json/operators/brava.js
+++ src/main/create-operator-json/operators/brava.js
@@ -8,17 +8,17 @@ import r6operators from "r6operators";
 let brava = new Operator(r6operators.brava, operatorId.brava, "Operation Commanding Force");
 
 brava.addCounterNode(operatorId.alibi, counterType.minor, "Brava's Krudge drone can hack Alibi's prismas");
-brava.addCounterNode(operatorId.aruni, counterType.hard, "Brava's Krudge drone can hack Aruni's surya gates")
-brava.addCounterNode(operatorId.echo, counterType.hard, "Brava's Krudge drone can hack Echo's yokai drone and controll it")
-brava.addCounterNode(operatorId.ela, counterType.hard, "Brava's Krudge drone can hack Ela's grzmot mines")
-brava.addCounterNode(operatorId.jager, counterType.hard, "Brava's Krudge drone can hack Jager's ADS")
-brava.addCounterNode(operatorId.kapkan, counterType.hard, "Brava's Krudge drone can hack Kapkan's EDDs")
-brava.addCounterNode(operatorId.maestro, counterType.hard, "Brava's Krudge drone can hack Maestro's evil eye and controll it")
-brava.addCounterNode(operatorId.melusi, counterType.hard, "Brava's Krudge drone can hack Melusi's banshee")
-brava.addCounterNode(operatorId.mozzie, counterType.minor, "Brava's Krudge drone can hack Mozzie's pests")
-brava.addCounterNode(operatorId.mute, counterType.major, "Brava's Krudge drone can hack Mute's jammers")
-brava.addCounterNode(operatorId.thorn, counterType.major, "Brava's Krudge drone can hack Thorn's razorblooms")
-brava.addCounterNode(operatorId.valkyrie, counterType.major, "Brava's Krudge drone can hack Valkyrie's black eyes")
+brava.addCounterNode(operatorId.aruni, counterType.hard, "Brava's Krudge drone can hack Aruni's surya gates");
+brava.addCounterNode(operatorId.echo, counterType.hard, "Brava's Krudge drone can hack Echo's yokai drone and controll it");
+brava.addCounterNode(operatorId.ela, counterType.hard, "Brava's Krudge drone can hack Ela's grzmot mines");
+brava.addCounterNode(operatorId.jager, counterType.hard, "Brava's Krudge drone can hack Jager's ADS");
+brava.addCounterNode(operatorId.kapkan, counterType.hard, "Brava's Krudge drone can hack Kapkan's EDDs");
+brava.addCounterNode(operatorId.maestro, counterType.hard, "Brava's Krudge drone can hack Maestro's evil eye and controll it");
+brava.addCounterNode(operatorId.melusi, counterType.hard, "Brava's Krudge drone can hack Melusi's banshee");
+brava.addCounterNode(operatorId.mozzie, counterType.minor, "Brava's Krudge drone can hack Mozzie's pests");
+brava.addCounterNode(operatorId.mute, counterType.hard, "Brava's Krudge drone can hack Mute's jammers");
+brava.addCounterNode(operatorId.thorn, counterType.hard, "Brava's Krudge drone can hack Thorn's razorblooms");
+brava.addCounterNode(operatorId.valkyrie, counterType.hard, "Brava's Krudge drone can hack Valkyrie's black eyes");
 
 
 export default brava
