85a6cb6bcb080697ea9ede3bda54162a09d0d20b
lily <lilyhuang-github@users.noreply.github.com>
2024-09-27 12:24:25 -0400

    tubarao counters

--- src/main/create-operator-json/operators/tubarao.js
+++ src/main/create-operator-json/operators/tubarao.js
@@ -7,4 +7,10 @@ import r6operators from "r6operators";
 
 let tubarao = new Operator(r6operators.tubarao, operatorId.tubarao, "Operation Deep Freeze");
 
+
+tubarao.addCounterNode(r6operators.thermite, counterType.hard, "Tubarao's Zoto Cannister can stop Thermite's Exothermic Charge from triggering.")
+tubarao.addCounterNode(r6operators.hibana, counterType.hard, "Tubarao's Zoto Cannister can stop Hibana's X-KARIOS pellets from triggering.")
+tubarao.addCounterNode(r6operators.ace, counterType.hard, "Tubarao's Zoto Cannister can stop Ace's SELMA Breaching Device from triggering.")
+tubarao.addCounterNode(r6operators.ram, counterType.hard, "Tubarao's Zoto Cannister can stop Ram's BU-GI from moving.")
+
 export default tubarao
