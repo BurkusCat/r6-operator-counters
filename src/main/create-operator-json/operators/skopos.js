dbd273fee3c61ab559354c02434ac40646b3003c
Michael Krebs <michael.krebs@xpublisher.com>
2025-04-10 11:13:09 +0200

    fix: update r6operators, set right counterNode arg

--- src/main/create-operator-json/operators/skopos.js
+++ src/main/create-operator-json/operators/skopos.js
@@ -7,6 +7,6 @@ import r6operators from "r6operators";
 
 let skopos = new Operator(r6operators.skopos, operatorId.skopos, "Operation Twin Shells");
 
-skopos.addCounterNode(r6operators.jackal, counterType.soft, "Skopos can switch between her V10 Pantheon Shells while being tracked, which will cause further red pings to be located at the now inactive shell.")
+skopos.addCounterNode(operatorId.jackal, counterType.soft, "Skopos can switch between her V10 Pantheon Shells while being tracked, which will cause further red pings to be located at the now inactive shell.")
 
 export default skopos
