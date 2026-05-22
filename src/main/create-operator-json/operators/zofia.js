70523cb8c2a68dec0cfc0ff019d5fb5123a2bc81
mchngc <107438905+mchngc@users.noreply.github.com>
2026-05-22 00:22:47 -0500

    Remove Zofia's resilience to Concussion effects

--- src/main/create-operator-json/operators/zofia.js
+++ src/main/create-operator-json/operators/zofia.js
@@ -7,9 +7,7 @@ import r6operators from "r6operators";
 
 let zofia = new Operator(r6operators.zofia, operatorId.zofia, "Operation White Noise");
 
-zofia.addCounterNode(operatorId.ela, counterType.soft, "Zofia is affected for a reduced amount of time by Ela's Grzmot mines.");
-zofia.addCounterNode(operatorId.echo, counterType.soft, "Zofia is affected for a reduced amount of time by Echo's Yokai drone.");
-zofia.addCounterNode(operatorId.clash, counterType.hard, "Zofia's Concussion Grenades can make Clash very vulnerable.");
+zofia.addCounterNode(operatorId.clash, counterType.hard, "Zofia's Concussion Grenades can make Clash very vulnerable. She can also destroy Clash's Shield with her Impact Grenades when it's mounted.");
 zofia.addCounterNode(operatorId.castle, counterType.hard, "Zofia's Impact Grenades can destroy Castle's Armor Panels.");
 zofia.addCounterNode(operatorId.maestro, counterType.hard, "Zofia's Impact Grenades can destroy Maestro's Evil Eye cameras.");
 zofia.addCounterNode(operatorId.melusi, counterType.hard, "Zofia's Impact Grenades can destroy Melusi's Banshee.");
