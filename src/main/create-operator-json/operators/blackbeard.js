ef5dbd2a09aa7c80eff13f01bd93266ad654556b
Ronan Burke <burkuscat@gmail.com>
2026-05-23 15:44:21 +0100

    Fix typo in Blackbeard's counter descriptions

--- src/main/create-operator-json/operators/blackbeard.js
+++ src/main/create-operator-json/operators/blackbeard.js
@@ -6,9 +6,7 @@ import Operator from '../core/operatorFactory.js';
 import r6operators from "r6operators";
 
 let blackbeard = new Operator(r6operators.blackbeard, operatorId.blackbeard, "Operation Dust Line/Collision Point");
-blackbeard.addCounterNode(operatorId.castle, counterType.hard, "Blackbeard's H.U.L.L Adaptable Shield can destroy Castle's Armor Panels with his pneumatic charges.");
-blackbeard.addCounterNode(operatorId.azami, counterType.hard, "Blackbeard's H.U.L.L Adaptable Shield can destroy Azami's Kiba Barriers with his pneumatic charges.");
-
-
+blackbeard.addCounterNode(operatorId.castle, counterType.hard, "Blackbeard's H.U.L.L. Adaptable Shield can destroy Castle's Armor Panels with his pneumatic charges.");
+blackbeard.addCounterNode(operatorId.azami, counterType.hard, "Blackbeard's H.U.L.L. Adaptable Shield can destroy Azami's Kiba Barriers with his pneumatic charges.");
 
 export default blackbeard
