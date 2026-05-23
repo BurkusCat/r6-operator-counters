125bdcc8787b0c2cf88ae70796af98c6ea76b209
Ronan Burke <burkuscat@gmail.com>
2026-05-23 15:48:41 +0100

    Update dokkaebi.js
    Thanks @mchngc for the contribution!

--- src/main/create-operator-json/operators/dokkaebi.js
+++ src/main/create-operator-json/operators/dokkaebi.js
@@ -5,7 +5,7 @@ import operatorId from '../core/operatorIdEnum.js';
 import Operator from '../core/operatorFactory.js';
 import r6operators from "r6operators";
 
-let dokkaebi = new Operator(r6operators.dokkaebi, operatorId.dokkaebi, "Operation White Noise/System Override");
+let dokkaebi = new Operator(r6operators.dokkaebi, operatorId.dokkaebi, "Operation White Noise");
 
 dokkaebi.addCounterNode(operatorId.maestro, counterType.hard, "Dokkaebi can use her Jegeo Payload to blow up Maestro's phone, cutting off his access to his Evil Eyes. She can also temporarily hack into the Evil Eye camera.");
 dokkaebi.addCounterNode(operatorId.echo, counterType.hard, "Dokkaebi can use her Jegeo Payload to blow up Echo's phone, cutting off his access to his Yokai drones. She can also temporarily hack into the Yokaicamera.");
